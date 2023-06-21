65f9943d0b67d6fe3094383d35112e9144abd77e
Maksym Kravchenko <kmaksym@gmail.com>
2023-06-20 22:49:40 -0700

    commit 2

--- src/components/imageGallery/impageGalleryItem.jsx
+++ src/components/imageGallery/impageGalleryItem.jsx
@@ -1,16 +1,15 @@
-import css from "./image.module.css";
-export const ImpageGalleryItem = ({ picture,isModalTrue, getId }) => {
+import css from "../imageGallery/image.module.css";
+export const ImpageGalleryItem = ({ picture, getId }) => {
   const { id, webformatURL } = picture;
     const handleClick = (id) => {
-        // console.log("Clicked item ID:", id);
         getId(id)
     };
     
   return (
     <li className={css.imagesLi} onClick={() => handleClick(id)}>
-      <a className={css.galleryLink}>
+      {/* <a className={css.galleryLink}> */}
         <img src={webformatURL} alt={id} className={css.imageCard} />
-      </a>
+      {/* </a> */}
     </li>
   );
 };
